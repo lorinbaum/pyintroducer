186572f3bc018579641e80b44f6104a24dd5d885
lorinbaum <lorin.baumgarten@outlook.com>
2024-07-31 13:27:19 +0200

    fixed any multi-line issues through preprocessing. Significant speedup

--- traceintro2.py
+++ traceintro2.py
@@ -6,14 +6,13 @@ from typing import List, Dict, Union
 from tokenize import tokenize, TokenError# , IndentationError
 from io import BytesIO
 
-# TODO: dictionary definitions print content first, then the variable, which reprints the dictionary because multiline
 # TODO: print existing class definition when redefining it
-# TODO: its printing whole classes for some reason
 # TODO: don't reintroduce just introduced parents
 # TODO: don't print empty class declarations
-# TODO: program runs absurdly slow. patternmatcher very slow. keeps calling the same functions (?)
-
-# errors = set()
+# TODO: following function calls UOp.const for the first time. the functions definition is skipped.
+    # the parent introduction following the call appears to come from nowhere to the unknowing reader
+        # def float4_expand_load(load, buf, ex, idx=UOp.const(dtypes.int, 0), idx2=None):
+    # maybe print the source of the call if its not printed already and explain what happened.
 
 class Tracer():
     def __init__(self, output_path):
@@ -23,11 +22,12 @@ class Tracer():
         # keep in mind a call event will first go to the decorator if there is one
         # Dict[filename:linenumber : str : Dict[parents: List[filename:linenumber : str], lines: List[line : str]]
         self.lexicon = {}
-        # stores all executed lines to avoid duplication
-        # new! stores all printed lines, not executed ones
+        # stores all printed lines to avoid duplication
         self.lines:Dict[str:List[int]] = {}
         # stores lines that are part of multilines that should be ignored in all cases
         self.skipmultilines:Dict[str:List[int]] = {}
+        # stores filepaths of file that ran through self.processMultilines
+        self.processedFiles = []
         # stores current filename:lineno on a call event (def or class statement), so following lines can find their parents easily
         self.parents = []
         # stores parents of the current line when tracing through line events, so that encountered functions and classes can get them assigned
@@ -39,26 +39,45 @@ class Tracer():
         self.spaced = True # beginning of line
 
 
+    def processMultilines(self, filepath:str):
+        """
+        reads file from top to bottom
+        tries to tokenize every line. if it fails, its a multiline statement. add lines until tokenize succeeds
+        every line in a multiline statement that isn't the first one is added to self.skipmultilines so it can later be ignored
+        """
+        if filepath in self.processedFiles: return
+        self.processedFiles.append(filepath)
+        self.skipmultilines[filepath] = []
+        with open(filepath, "r") as f:
+            i = 0
+            lines = f.readline()
+            while True:
+                i += 1
+                if lines == "": break
+                try:
+                    list(tokenize(BytesIO(lines.encode("utf-8")).readline))
+                except TokenError as e:
+                    if "EOF in multi-line" in str(e):
+                        self.skipmultilines[filepath].append(i + 1)
+                        lines += f.readline()
+                        continue
+                    else: raise(e)
+                lines = f.readline()
+
     def advance(self, filename, lineno, count):
         """
         advances `count` lines from filename:lineno, skipping parts of multiline statements and returns the new line
         advance(filename, lineno, 0) returns full current line with multilines if any
         """
         assert count >= 0
-        lines = linecache.getline(filename, lineno)
-        if lines.strip() == "": return "", []
-        i = 1
-        while count + 1:
-            try:
-                list(tokenize(BytesIO(lines.encode("utf-8")).readline))
-            except TokenError as e:
-                if str(e).startswith("('EOF in multi-line "):
-                    lines += linecache.getline(filename, lineno + i)
-                    i += 1
-                    continue
-            count -= 1
-            if count > 0: lines = linecache.getline(filename, lineno + i)
-        line, *multilines = [ln + "\n" for ln in lines.split("\n") if ln != ""]
+        assert lineno not in self.skipmultilines[filename]
+        newlineno = lineno
+        line:str
+        multilines = []
+        for i in range(count + 1):
+            if i == count: line = linecache.getline(filename, newlineno)
+            while (newlineno := newlineno + 1) in self.skipmultilines[filename]:
+                if i == count: multilines.append(linecache.getline(filename, newlineno))
         return line, multilines
 
     def recede(self, filename, lineno, count):
@@ -66,39 +85,14 @@ class Tracer():
         recedes `count` lines from filename:lineno, skipping parts of multiline statements and returns the new line
         """
         assert count > 0
-        lines = linecache.getline(filename, lineno - 1)
-        if lines.strip() == "": return "", []
-        i = 2
-        magiclines = 0
-        while count:
-            try:   
-                # print("new docstring")
-                # print(f"lines when receding through {filename}:{lineno}:{count=}: {lines}")
-                list(tokenize(BytesIO(lines.encode("utf-8")).readline))
-            except TokenError as e:
-                # if e not in errors:
-                #     print(e)
-                #     errors.add(e)
-                if str(e).startswith("('EOF in multi-line "):
-                    # if it finds a line with explicit line joining at the end, but the next line is not in lines (it was judges as ok),
-                    # it should take that next and see if it tokenizes, error if not
-                    if lines.endswith("\\\n") and (nextln := linecache.getline(filename, lineno + magiclines)) not in lines:
-                        # print("magic method activate")
-                        lines = lines + nextln
-                        magiclines += 1
-                        continue
-                    lines = linecache.getline(filename, lineno - i) + lines
-                    i += 1
-                    continue
-            except IndentationError as e:
-                # ignore indentation errors when inside docstrings
-                if lines.strip().endswith('"""') or lines.strip().endswith("'''"):
-                    lines = linecache.getline(filename, lineno - i) + lines
-                    i += 1
-                    continue
-            count -= 1
-            if count > 0: lines = linecache.getline(filename, lineno - i)
-        line, *multilines = [ln + "\n" for ln in lines.split("\n") if ln != ""]
+        assert lineno not in self.skipmultilines[filename]
+        newlineno = lineno
+        line:str
+        multilines = []
+        for i in range(count):
+            while (newlineno := newlineno - 1) in self.skipmultilines[filename]:
+                if i == count - 1: multilines.append(linecache.getline(filename, newlineno))
+            if i == count - 1: line = linecache.getline(filename, newlineno)
         return line, multilines
 
     def singleSpace(self, force=False):
@@ -140,21 +134,11 @@ class Tracer():
     def trace_dispatch(self, frame, event, arg):
         filename = frame.f_code.co_filename
         lineno = frame.f_lineno
-        if "python3" not in filename:
+        if "python3" not in filename and not filename.startswith("<"):
+            if filename not in self.processedFiles: self.processMultilines(filename)
             if filename not in self.lines: self.lines[filename] = []
-            if filename not in self.skipmultilines: self.skipmultilines[filename] = []
-            # call events need to go through to let later lines know where they come from
-            # same for return events
-            # if lineno not in self.lines[filename] or event!="line":
             if lineno not in self.skipmultilines[filename]:
                 line, multilines = self.advance(filename, lineno, 0)
-                # print([line, *multilines])
-                # if event == "line":
-                    # for i in range(len(multilines) + 1):
-                for i in range(len(multilines)):
-                    # self.lines[filename].add(lineno+i)
-                    self.skipmultilines[filename].append(lineno+i+1)
-                    # print(f"added line {filename}{lineno + i} to visited ones")
                 if line.strip() != "":
                     filename_short = filename.split("tinygrad/")[-1] if "tinygrad" in filename else filename
                     print(f"{filename_short:40}:{lineno:6}", end="\r")
@@ -168,10 +152,8 @@ class Tracer():
                             # only print def and class lines if they are identical to the current parent
                             if line.strip().startswith("@"):
                                 while line.strip().startswith("@"):
-                                    # line = linecache.getline(filename, lineno + i + len(multilines))
                                     line, multilines = self.advance(filename, lineno, 1)
                                     lineno = lineno + 1 + len(multilines)
-                                lineno = lineno - 1 - len(multilines) # to get last line, not next
                             if line.strip().startswith("def ") or line.strip().startswith("class "):
                                 self.parentsIntroduced = False
                                 self.parents.append(f"{filename}:{lineno}")
@@ -201,23 +183,6 @@ class Tracer():
                                             self.prospectiveParents.pop()
                                         self.prospectiveParents[-1] = {"name": f"{filename}:{lineno}", "indent": indent}
                                 if (key :=f"{filename}:{lineno}") not in self.lexicon: self.lexicon[key] = {"parents": [p["name"] for p in self.prospectiveParents[:-1]], "lines": []}
-                                # if (key :=f"{filename}:{lineno}") not in self.lexicon:
-                                #     parents = [] # higher indices = more distant parent
-                                #     indent = len(whitespace) if (whitespace:=re.search("^(\s*)", line)[1]) else 0
-                                #     newlineno = lineno
-                                #     while indent > 0:
-                                #         # newline = linecache.getline(filename, lineno - i)
-                                #         newline, newmultilines = self.recede(filename, newlineno, 1)
-                                #         newlineno = newlineno - 1 - len(newmultilines)
-                                #         if newline.strip() != "":
-                                #             newIndent = len(whitespace) if (whitespace:=re.search("^(\s*)", newline)[1]) else 0
-                                #             if newIndent < indent:
-                                #                 parents.append(f"{filename}:{newlineno}")
-                                #                 indent = newIndent
-                                #     self.lexicon[key] = {
-                                #         "parents": parents,
-                                #         "lines": []
-                                #     }
                             if filename == script_path:
                                 self.write(filename, lineno, [line, *multilines])
                                 self.spaced = False
