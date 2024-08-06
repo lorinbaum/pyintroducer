b6a22685a4428f812fcc2bbb2024eb418cdfee2b
lorinbaum <lorin.baumgarten@outlook.com>
2024-08-06 14:46:28 +0200

    add indentation that reflects callstack depth

--- traceraw.py
+++ traceraw.py
@@ -6,27 +6,33 @@ class Tracer():
     def __init__(self):
         self.execlines = []
         self.prev_filename = None
+        self.indent = 0
+
+        self.calls, self.retus = 0, 0
 
     def trace_dispatch(self, frame, event, arg):
         filename = frame.f_code.co_filename
         lineno = frame.f_lineno
         line = linecache.getline(filename, lineno)
-        if line != "" and \
-        "python3" not in filename:
+        if line.strip() != "" and \
+        "python3" not in filename and not filename.startswith("<"):
             if event == "call":
-                self.execlines.append(f"call: {line}")
-            if event == "return":
-                self.execlines.append(f"retu: {line}")
-            if event == "line":
-                self.execlines.append(f"line: {line}")
+                self.execlines.append(f"{self.indent * ' '}call: {line}")
+                self.indent += 2
+                self.calls += 1
+            elif event == "return":
+                self.execlines.append(f"{self.indent * ' '}retu: {line}")
+                self.indent -= 2
+                self.retus += 1
+            elif event == "line":
+                self.execlines.append(f"{self.indent * ' '}line: {line}")
                 
         return self.trace_dispatch
         
 
-script_path = sys.argv[1]
+# script_path = sys.argv[1]
+script_path = "test.py"
 tracer = Tracer()
-with open(script_path, "r") as script_file:
-    script = script_file.read()
 
 sys.settrace(tracer.trace_dispatch)
 runpy.run_path(script_path, run_name="__main__")
@@ -36,3 +42,5 @@ output_path = f"{script_path}_trace.txt"
 print(f"writing to {output_path}")
 with open(output_path, "w") as output_file:
     output_file.write("".join(tracer.execlines))
+
+print(tracer.calls, tracer.retus)
