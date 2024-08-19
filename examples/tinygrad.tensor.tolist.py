303ed6bdfb47a75deb420bc1ec93205c52c839c9
lorinbaum <lorin.baumgarten@outlook.com>
2024-08-19 09:26:20 +0200

    write changed local and global variables

--- examples/tinygrad.tensor.tolist.py
+++
@@ -1,3 +0,0 @@
-from tinygrad.tensor import Tensor
-a = (Tensor([1,2,3], device="CLANG") + 2)
-a.tolist()
--- traceraw.py
+++ traceraw.py
@@ -3,12 +3,21 @@ import linecache
 import runpy
 
 class Tracer():
-    def __init__(self):
+    def __init__(self, opath):
+        self.f = open(opath, "w")
         self.execlines = []
         self.prev_filename = None
         self.indent = 0
+        
+        self.namespaces, self.namespace = {}, None
 
-        self.calls, self.retus = 0, 0
+    def write(self, event, line, filename, lineno, glob, loca):
+        globstring = str(glob).replace("\n", "\\n")
+        try:
+            locastring = str(loca).replace("\n", "\\n")
+        except: locastring = f"repr failed"
+        self.f.write(f"{self.indent * ' '}{event[:4]:4}: {line.rstrip():{200 - self.indent}}{filename[-30:]}:{lineno:6}    G: {globstring}     L: {locastring}\n")
+
 
     def trace_dispatch(self, frame, event, arg):
         filename = frame.f_code.co_filename
@@ -16,31 +25,31 @@ class Tracer():
         line = linecache.getline(filename, lineno)
         if line.strip() != "" and \
         "python3" not in filename and not filename.startswith("<"):
-            if event == "call":
-                self.execlines.append(f"{self.indent * ' '}call: {line}")
-                self.indent += 2
-                self.calls += 1
-            elif event == "return":
-                self.execlines.append(f"{self.indent * ' '}retu: {line}")
-                self.indent -= 2
-                self.retus += 1
-            elif event == "line":
-                self.execlines.append(f"{self.indent * ' '}line: {line}")
+            
+            if (name := frame.f_globals["__name__"]) != self.namespace: self.namespace = name
+            if name not in self.namespaces: self.namespaces[name] = {"glob": {}, "loca": {}}
+
+            newglob = {key:value for key, value in frame.f_globals.items() if key not in ["__builtins__"] and (key not in self.namespaces[name]["glob"] or id(self.namespaces[name]["glob"][key]) != id(value))}
+            newloca = {key:value for key, value in frame.f_locals.items() if key not in ["__builtins__", *newglob.keys(), *self.namespaces[name]["glob"].keys()] and (key not in self.namespaces[name]["loca"] or id(self.namespaces[name]["loca"][key]) != id(value))}
+            
+            self.namespaces[name]["glob"] = frame.f_globals
+            self.namespaces[name]["loca"] = frame.f_locals
+
+            self.write(event, line, filename, lineno, newglob, newloca)
+            if event == "call": self.indent += 2
+            elif event == "return": self.indent -= 2
                 
         return self.trace_dispatch
         
 
-# script_path = sys.argv[1]
-script_path = "test.py"
-tracer = Tracer()
+script_path = sys.argv.pop(1)
+# script_path = "test.py"
+output_path = f"{script_path}_traceraw.txt"
+tracer = Tracer(output_path)
 
 sys.settrace(tracer.trace_dispatch)
 runpy.run_path(script_path, run_name="__main__")
 sys.settrace(None)
 
-output_path = f"{script_path}_traceraw.txt"
 print(f"writing to {output_path}")
-with open(output_path, "w") as output_file:
-    output_file.write("".join(tracer.execlines))
-
-print(tracer.calls, tracer.retus)
+tracer.f.close()
